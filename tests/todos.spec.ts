3e3f494e238b6bae3c98de5fddbde56e09dbd965
Zaruhi-Antonyan <zaraantonyan@yahoo.com>
2026-08-14 01:26:09 +0400

    14/08/26

--- tests/todos.spec.ts
+++ tests/todos.spec.ts
@@ -1,18 +1,19 @@
-import { test, expect, request } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
 const BASE_URL = 'https://jsonplaceholder.typicode.com';
 const todoId = 1;
 
 test.describe('TODOS API Testing', () => {
     
-
     test(`GET /todos`, async ({ request }) => {
+        
         const response = await request.get(`${BASE_URL}/todos`);
         
         expect(response.ok()).toBe(true);
         expect(response.status()).toBe(200);
         
         const data = await response.json();
+
         expect(Array.isArray(data)).toBe(true);
         expect(data.length).toBe(200);
         
@@ -30,42 +31,108 @@ test.describe('TODOS API Testing', () => {
             expect(obj.userId).toBeGreaterThan(0);
             expect(obj.id).toBeGreaterThan(0);
             expect(obj.title.length).toBeGreaterThan(0);
+
+            // obj id
+            let count = 0;
+            for(let obj2 of data){
+                if(obj.id === obj2.id){
+                    count++;
+                }
+            }
+            expect(count, `The count should be 1 for id ${obj.id}, but now it is ${count}`).toBe(1);
         }
+    });
+
+    test(`GET /todos/${todoId}`, async ({ request }) => {
+        
+        const response = await request.get(`${BASE_URL}/todos/${todoId}`);
+        
+        expect(response.ok()).toBe(true);
+        expect(response.status()).toBe(200);
+        
+        const data = await response.json();
+
+        expect(typeof data).toBe('object');
+        
+        expect(data.userId).toBeDefined();
+        expect(data.id).toBeDefined();
+        expect(data.title).toBeDefined();
+        expect(data.completed).toBeDefined();
+        
+        expect(typeof data.userId).toBe('number');
+        expect(typeof data.id).toBe('number');
+        expect(typeof data.title).toBe('string');
+        expect(typeof data.completed).toBe('boolean');
+        
+        expect(data.userId).toBeGreaterThan(0);
+        expect(data.id).toBe(todoId);
+        expect(data.title.length).toBeGreaterThan(0);
     });
 
     test(`POST /todos`, async ({ request }) => {
         
-        const new_todo = {
-            userId: 1,
-            title: "New Todo 1",
-            completed: true
-        }
+        let userId: number;
 
-        const response = await request.post(`${BASE_URL}/todos`, {
-            data: new_todo
+        await test.step('Get userId from users list', async() => {
+            const response_users = await request.get('https://jsonplaceholder.typicode.com/users');
+            expect(response_users.ok()).toBeTruthy();
+            expect(response_users.status()).toBe(200);
+            const data_users = await response_users.json();
+            expect(Array.isArray(data_users)).toBeTruthy();
+            expect(data_users.length).toBeGreaterThan(0);
+            userId = data_users[0].id;
+        })
+
+        await test.step('Create new todo', async() => {
+
+            const new_todo = {
+                userId: userId,
+                title: "New Todo 1",
+                completed: true
+            }
+
+            const response = await request.post(`${BASE_URL}/todos`, {
+                data: new_todo
+            })
+
+            expect(response.ok()).toBeTruthy();
+            expect(response.status()).toEqual(201);
+
+            const data = await response.json();
+
+            expect(data.userId).toBe(new_todo.userId);
+            expect(data.id).toBeDefined();
+            expect(data.title).toBe(new_todo.title);
+            expect(data.completed).toBe(new_todo.completed);
         });
-
-        expect(response.ok()).toBeTruthy();
-        expect(response.status()).toEqual(201);
-
-        const data = await response.json();
-        expect(data.userId).toBe(new_todo.userId);
-        expect(data.id).toBeDefined();
-        expect(data.title).toBe(new_todo.title);
-        expect(data.completed).toBe(new_todo.completed);
     });
+        
 
     test(`PUT /todos/${todoId}`, async ({ request }) => {
         
-        const updated_todo = {
-            userId: 1,
-            title: "UPDATE TODO TITLE",
-            completed: true
-        }
-
-        const response = await request.put(`${BASE_URL}/todos/${todoId}`, {
-            data: updated_todo
+        let userId: number;
+        
+        await test.step('Put userId from users list', async() => {
+            const response_users = await request.get('https://jsonplaceholder.typicode.com/users');
+            expect(response_users.ok()).toBeTruthy();
+            expect(response_users.status()).toBe(200);
+            const data_users = await response_users.json();
+            expect(Array.isArray(data_users)).toBeTruthy();
+            expect(data_users.length).toBeGreaterThan(0);
+            userId = data_users[0].id;
         });
+
+        await test.step('Create new todo', async() => {
+
+            const updated_todo = {
+                userId: userId,
+                title: "New Todo 1",
+                completed: true
+            }
+
+            const response = await request.put(`${BASE_URL}/todos/${todoId}`, {
+                data: updated_todo
+            });
 
         expect(response.ok()).toBeTruthy();
         expect(response.status()).toEqual(200);
@@ -75,7 +142,10 @@ test.describe('TODOS API Testing', () => {
         expect(data.id).toBe(todoId);
         expect(data.title).toBe(updated_todo.title);
         expect(data.completed).toBe(updated_todo.completed);
+
+        });
     });
+    
 
     test(`PATCH /todos/${todoId}`, async ({ request }) => {
         
@@ -94,7 +164,7 @@ test.describe('TODOS API Testing', () => {
         expect(data.userId).toBe(1);
         expect(data.id).toBe(todoId);
         expect(data.title).toBe(partially_updated_todo.title);
-        expect(data.completed).toBe(false);
+        expect(data.completed).toBeDefined();
     });
 
     test(`DELETE /todos/${todoId}`, async ({ request }) => {
